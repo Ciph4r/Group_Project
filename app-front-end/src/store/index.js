71c5da3278d36fde87d679fe752e2a8b0bf16cd2
Dave.L <59891341+Ciph4r@users.noreply.github.com>
2020-11-12 12:15:57 -0500

    added temp store

---
+++ app-front-end/src/store/index.js
@@ -0,0 +1,8 @@
+import {createStore , applyMiddleware} from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension';
+import {allReducers} from './reducer/combineReducer'
+import logger from 'redux-logger'
+
+
+
+export const store = createStore(allReducers , composeWithDevTools(applyMiddleware(logger)))           
