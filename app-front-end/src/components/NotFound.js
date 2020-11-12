94e80e7855d0b95bf6a1a789269b22e74a1ae7cf
Dave.L <59891341+Ciph4r@users.noreply.github.com>
2020-11-11 23:55:42 -0500

    added side nav

--- app-front-end/src/components/NotFound.js
+++ app-front-end/src/components/NotFound.js
@@ -4,7 +4,7 @@ import React from 'react'
 export default function NotFound () {
     return (
         <div>
-            <h1>
+            <h1 style = {{color: 'red'}} >
                 404 NOT FOUND
             </h1>
         </div>
--- app-front-end/src/components/header/LeftNav.js
+++ app-front-end/src/components/header/LeftNav.js
@@ -1,10 +1,75 @@
 import React from 'react';
 
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import FormatListBulletedIcon from '@material-ui/icons/FormatListBulleted';
+import Divider from '@material-ui/core/Divider';
+import { Link } from 'react-router-dom';
+
+const useStyles = makeStyles({
+    list: {
+      width: 250,
+    },
+    fullList: {
+      width: 'auto',
+    },
+  });
+
 export default function LeftNav () {
+    const classes = useStyles();
+    const [state, setState] = React.useState({
+      left: false,
+    });
+  
+    const toggleDrawer = (anchor, open) => (event) => {
+      if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+        return;
+      }
+  
+      setState({ ...state, [anchor]: open });
+    };
+  
+    const list = (anchor) => (
+      <div
+        className={clsx(classes.list)}
+        role="presentation"
+        onClick={toggleDrawer(anchor, false)}
+        onKeyDown={toggleDrawer(anchor, false)}
+        // How low you want it
+        style = {{marginTop: '500px' , fontSize: '30px'}}
+      >
+        <List>
+        <Link to="/listing">Listing</Link>
+          <Divider />
+          <Link to="/booking">Booking</Link>
+          <Divider />
+          <Link to="/favorites">Favorties</Link>
+        </List>
+
+        {/* <Divider />
+        <List>
+          {['All mail', 'Trash', 'Spam'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List> */}
+
+      </div>
+    );
+  
     return (
-        <div>
-            <h1>Left Nav</h1>
-        </div>
-        
-    )
+      <div>
+          <React.Fragment key={'left'}>
+            <Button onClick={toggleDrawer('left', true)}><FormatListBulletedIcon/></Button>
+            <Drawer anchor={'left'} open={state['left']} onClose={toggleDrawer('left', false)}>
+              {list('left')}
+            </Drawer>
+          </React.Fragment>
+      </div>
+    );
 }
