075bfed3e48cb3e25b5bb40d01a55d1ebde6ac44
Dave.L <59891341+Ciph4r@users.noreply.github.com>
2020-11-12 12:36:01 -0500

    added temp store

---
+++ app-front-end/src/store/reducer/anotherTempReducer.js
@@ -0,0 +1,7 @@
+const intstate = {
+    num:['5','6','7','8','9'],
+  }
+
+  export const tempReducer2 = (state = intstate , action) => {
+    return state
+  }
--- app-front-end/src/store/reducer/combineReducer.js
+++ app-front-end/src/store/reducer/combineReducer.js
@@ -1,5 +1,7 @@
 import {tempReducer} from './tempReducer'
 import {combineReducers} from 'redux'
+import {tempReducer2} from './anotherTempReducer'
 
-export const allReducers = combineReducers({temp: tempReducer})
+
+export const allReducers = combineReducers({temp: tempReducer , anotherStateStorage: tempReducer2})
 
