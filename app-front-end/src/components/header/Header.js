3053213fc9a51468db7d9f47a22ebf682bce39f2
[nick_fasulo] <[nick.fasulo@codeimmersives]>
2020-11-12 15:15:35 -0500

    pull

--- app-front-end/src/components/header/Header.js
+++ app-front-end/src/components/header/Header.js
@@ -1,15 +1,14 @@
-import React from 'react'
-import Account from './Account'
-import Inbox from './Inbox'
-import LeftNav from './LeftNav'
+import React from 'react';
+import Account from './Account';
+import Inbox from './Inbox';
+import LeftNav from './LeftNav';
 
-
-export default function Header () {
-    return (
-        <div>
-            <LeftNav/>
-            <Account/>
-            <Inbox/>
-        </div>
-    )
+export default function Header() {
+  return (
+    <div className="header">
+      <LeftNav />
+      <Account />
+      <Inbox />
+    </div>
+  );
 }
