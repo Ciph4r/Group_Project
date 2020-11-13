1f1ebc6fd5b032ce06696344889d7d2b0c08db21
Dave.L <59891341+Ciph4r@users.noreply.github.com>
2020-11-13 01:46:14 -0500

    added history bar

--- app-front-end/src/components/historyBar/HistoryBar.js
+++ app-front-end/src/components/historyBar/HistoryBar.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import {React , useState} from 'react'
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 
 export default function HistoryBar () {
+    const [hover , setHover] = useState(false)
+
+    const handleMouseHover = () => setHover(!hover)
+      
+      
+
+
     return (
         <div className = 'history-bar'>
-            <h1> History bar</h1>
+            
+            <div className = 'content' onMouseEnter={handleMouseHover} onMouseLeave={handleMouseHover}>
+                <ExpandLessIcon style={{ fontSize: '90' , visibility: hover ? 'visible' : 'hidden' }} />
+                <div className= 'items'>
+                    <h1>hover over me</h1>
+                </div>
+                <ExpandMoreIcon style={{ fontSize: '90' , visibility: hover ? 'visible' : 'hidden'}} />
+            </div>
+            
         </div>
     )
 }
