16bf61af57159075297ba665749a4b8fd0046986
[nick_fasulo] <[nick.fasulo@codeimmersives]>
2020-11-13 12:08:52 -0500

    changed header css

--- app-front-end/src/components/header/Header.js
+++ app-front-end/src/components/header/Header.js
@@ -2,17 +2,18 @@ import React from 'react';
 import Account from './Account';
 import Inbox from './Inbox';
 import LeftNav from './LeftNav';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
     <div className="top-bar">
       <div className="bar">
-        <div className = 'name'>
+        <Link to="/" className="name">
           <h1>GroundRnR</h1>
-        </div>
-        <div>
-          <Account/>
-          <Inbox/>
+        </Link>
+        <div className="top-icons">
+          <Account />
+          <Inbox />
         </div>
       </div>
       <div className="links">
