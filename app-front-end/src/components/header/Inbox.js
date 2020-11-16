3e28d2190eaeb98f2d9e8e9172835f02c9827f83
Dave.L <59891341+Ciph4r@users.noreply.github.com>
2020-11-16 16:18:38 -0500

    addded notification badge

--- app-front-end/src/components/header/Inbox.js
+++ app-front-end/src/components/header/Inbox.js
@@ -1,23 +1,85 @@
-import {React , useState} from 'react'
+import {React , useState ,  useEffect} from 'react'
 import EmailIcon from '@material-ui/icons/Email';
 import {MailModal} from './MailModal'
 
 
+const tempData = [
+    {
+         name:'Dave',
+         date:'11:45 PM',
+         read: false,
+         message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+         img: 'https://c7.alamy.com/comp/P9MYWR/man-avatar-profile-P9MYWR.jpg'
+
+    },
+    {
+         name:'Jim',
+         date:'11:45 PM',
+         read: false,
+         message: 'Hello World 2',
+         img: 'https://c7.alamy.com/comp/P9MYWR/man-avatar-profile-P9MYWR.jpg'
+    },
+    {
+         name:'Jim',
+         date:'11:45 PM',
+         read: true,
+         message: 'Hello World 2',
+         img: 'https://c7.alamy.com/comp/P9MYWR/man-avatar-profile-P9MYWR.jpg'
+    },
+    {
+         name:'Jim',
+         date:'11:45 PM',
+         read: true,
+         message: 'Hello World 2',
+         img: 'https://c7.alamy.com/comp/P9MYWR/man-avatar-profile-P9MYWR.jpg'
+    },
+    {
+         name:'Jim',
+         date:'11:45 PM',
+         read: true,
+         message: 'Hello World 2',
+         img: 'https://c7.alamy.com/comp/P9MYWR/man-avatar-profile-P9MYWR.jpg'
+    },
+    {
+         name:'Jim',
+         date:'11:45 PM',
+         read: true,
+         message: 'Hello World 2',
+         img: 'https://c7.alamy.com/comp/P9MYWR/man-avatar-profile-P9MYWR.jpg'
+    },
+
+]
+
 
 
 export default function Inbox () {
-
+    const [mailCount, setMailCount] =useState(0);
     const [openMail,setOpenMail] = useState(false)
-
     const closeMailHandler = () => setOpenMail(false)
     const openMailHandler = () => setOpenMail(true)
 
+    
+    useEffect(() => {
+        let num = 0
+        for (let i = 0; i < tempData.length; i++) {
+            if (tempData[i].read === true){
+                num++
+            }
+          }
+          setMailCount(num)
+      });
+
+      
     return (
         <div>
-            <MailModal closeMailHandler= {closeMailHandler} openMail={openMail}>
+            <MailModal closeMailHandler= {closeMailHandler} openMail={openMail} tempData = {tempData}>
 
             </MailModal>
-            <EmailIcon style={{ color: '#d9d9d9',cursor: 'pointer' }} onClick={openMailHandler} />
+            <div href="#" class="notification">
+                <EmailIcon style={{ color: '#d9d9d9',cursor: 'pointer' }} onClick={openMailHandler}/>
+    <span class="badge">{mailCount}</span>
+            </div>
+            {/* <EmailIcon style={{ color: '#d9d9d9',cursor: 'pointer' }} onClick={openMailHandler} /> */}
         </div>
 
     )
--- app-front-end/src/components/header/MailModal.js
+++ app-front-end/src/components/header/MailModal.js
@@ -3,56 +3,9 @@ import Modal from 'react-modal'
 import '../../css/mailModal.scss'
 import {InboxItem} from './InboxItem'
 
-const tempData = [
-     {
-          name:'Dave',
-          date:'11:45 PM',
-          read: false,
-          message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-          img: 'https://c7.alamy.com/comp/P9MYWR/man-avatar-profile-P9MYWR.jpg'
-
-     },
-     {
-          name:'Jim',
-          date:'11:45 PM',
-          read: true,
-          message: 'Hello World 2',
-          img: 'https://c7.alamy.com/comp/P9MYWR/man-avatar-profile-P9MYWR.jpg'
-     },
-     {
-          name:'Jim',
-          date:'11:45 PM',
-          read: true,
-          message: 'Hello World 2',
-          img: 'https://c7.alamy.com/comp/P9MYWR/man-avatar-profile-P9MYWR.jpg'
-     },
-     {
-          name:'Jim',
-          date:'11:45 PM',
-          read: true,
-          message: 'Hello World 2',
-          img: 'https://c7.alamy.com/comp/P9MYWR/man-avatar-profile-P9MYWR.jpg'
-     },
-     {
-          name:'Jim',
-          date:'11:45 PM',
-          read: true,
-          message: 'Hello World 2',
-          img: 'https://c7.alamy.com/comp/P9MYWR/man-avatar-profile-P9MYWR.jpg'
-     },
-     {
-          name:'Jim',
-          date:'11:45 PM',
-          read: true,
-          message: 'Hello World 2',
-          img: 'https://c7.alamy.com/comp/P9MYWR/man-avatar-profile-P9MYWR.jpg'
-     },
 
 
-]
-
-
-export const MailModal = ({openMail, closeMailHandler}) => {
+export const MailModal = ({openMail, closeMailHandler , tempData}) => {
     return (
             <Modal isOpen={openMail} className='modal' 
             shouldCloseOnOverlayClick={true}
@@ -65,7 +18,6 @@ export const MailModal = ({openMail, closeMailHandler}) => {
                          {tempData.map((data, key) => (
                              <InboxItem key = {key}
                               data = {data}
-                              
                              />
                          ))}
                  </div>
