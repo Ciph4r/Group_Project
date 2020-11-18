c33c86a96175049d94959e574e305cf28a883a4c
Dave.L <59891341+Ciph4r@users.noreply.github.com>
2020-11-18 14:54:43 -0500

    updated css and added listing

--- app-front-end/src/components/CreateListingModal.js
+++ app-front-end/src/components/CreateListingModal.js
@@ -2,9 +2,30 @@ import {React , useState} from 'react'
 import Modal from 'react-modal'
 import TextField from '@material-ui/core/TextField';
 import '../css/loginRegisterModal.scss'
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import { makeStyles } from '@material-ui/core/styles';
 
+
+
+
+const useStyles = makeStyles((theme) => ({
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 120,
+    },
+    selectEmpty: {
+      marginTop: theme.spacing(2),
+    },
+  }));
 
 export const CreateListingModal = ({showCreateModal}) => {
+    const classes = useStyles();
+
+
 
     return (
         <Modal isOpen={showCreateModal} className='create-listing-modal' 
@@ -15,9 +36,65 @@ export const CreateListingModal = ({showCreateModal}) => {
                      <span className='close-modal-btn' onClick={() => {}}><h2>x</h2></span>
             </div>
             <div className='modal-content'>
-                <TextField id="standard-basic" label="FirstName" />
-                <TextField id="standard-basic" label="LastName" />
+                <TextField id="standard-basic" label="Maker" />
+                <TextField id="standard-basic" label="Model" />
                 <hr/>
+                <div className='car-option'>
+                    <FormControl className={classes.formControl}>
+                        <InputLabel id="demo-simple-select-label">Year</InputLabel>
+                        <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            value={1}
+                            onChange={()=>{}}
+                            >
+                            <MenuItem value={10}>Ten</MenuItem>
+                            <MenuItem value={20}>Twenty</MenuItem>
+                            <MenuItem value={30}>Thirty</MenuItem>
+                        </Select>
+                    </FormControl>
+                    
+                    <FormControl className={classes.formControl}>
+                        <InputLabel id="demo-simple-select-label">Year</InputLabel>
+                        <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            value={1}
+                            onChange={()=>{}}
+                            >
+                            <MenuItem value={10}>Ten</MenuItem>
+                            <MenuItem value={20}>Twenty</MenuItem>
+                            <MenuItem value={30}>Thirty</MenuItem>
+                        </Select>
+                    </FormControl>
+
+                        <InputLabel id="demo-simple-select-label">Door #</InputLabel>
+                        <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            value={1}
+                            onChange={()=>{}}
+                            >
+                            <MenuItem value={10}>Ten</MenuItem>
+                            <MenuItem value={20}>Twenty</MenuItem>
+                            <MenuItem value={30}>Thirty</MenuItem>
+                        </Select>
+
+                        <InputLabel id="demo-simple-select-label">Size</InputLabel>
+                        <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            value={1}
+                            onChange={()=>{}}
+                            >
+                            <MenuItem value={10}>Ten</MenuItem>
+                            <MenuItem value={20}>Twenty</MenuItem>
+                            <MenuItem value={30}>Thirty</MenuItem>
+                        </Select>
+                   
+
+
+                </div>
                 <TextField id="standard-basic" label="Email" />
                 <hr/>
                 <TextField id="standard-basic" label="Password" />
