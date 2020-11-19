aa0af1ad5fbef963d0818495dce1a2534f65763b
Dave.L <59891341+Ciph4r@users.noreply.github.com>
2020-11-19 11:42:14 -0500

    updating edit modal

--- app-front-end/src/components/Listing.js
+++ app-front-end/src/components/Listing.js
@@ -5,6 +5,12 @@ import Divider from '@material-ui/core/Divider';
 import 'react-calendar/dist/Calendar.css';
 import {CreateListingModal} from './CreateListingModal'
 
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+
+
 const tempData = [
     {
       img:
@@ -44,6 +50,17 @@ const tempData = [
 
 const ListingCard = (data) => {
     const [value, onChange] = useState(new Date());
+
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handleClick = (event) => {
+      setAnchorEl(event.currentTarget);
+    };
+  
+    const handleClose = () => {
+      setAnchorEl(null);
+    };
+
     let {img,year,make,model,price} =data
 
     return(
@@ -51,21 +68,41 @@ const ListingCard = (data) => {
             <div className='calender'>
                 <Calendar onChange={onChange}value={value}/>
             </div>
-            <div className='img'>
-                <img src={img} alt="..." />
+            <div className='card-group'>
+              <div className='img'>
+                  <img src={img} alt="..." />
+              </div>
+              <div className='card-content'>
+                  <div className="car-info">
+                      <h3 className="year-make">
+                          {year} {make}
+                      </h3>
+                      <h3 className="model">{model}</h3>
+                  </div>
+                  <div className="car-info">
+                      <h4 className="price">{price}</h4>
+                      <span>per day</span>
+                  </div>
+              </div>
             </div>
-            <div className='card-content'>
-                <div className="car-info">
-                    <h3 className="year-make">
-                        {year} {make}
-                    </h3>
-                    <h3 className="model">{model}</h3>
+       
+            <div className='msg-options'>
+                    <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+                        <MoreHorizIcon fontSize="large"/>
+                    </Button>
+                    <Menu
+                        id="simple-menu"
+                        anchorEl={anchorEl}
+                        keepMounted
+                        open={Boolean(anchorEl)}
+                        onClose={handleClose}
+                    >
+                        <MenuItem onClick={handleClose}>Delete</MenuItem>
+                        {/* <MenuItem onClick={handleClose}>...</MenuItem>
+                        <MenuItem onClick={handleClose}>...</MenuItem> */}
+                    </Menu>    
                 </div>
-                <div className="car-info">
-                    <h4 className="price">{price}</h4>
-                    <span>per day</span>
-                </div>
-            </div>
+
         </div>
     )
 }
@@ -79,9 +116,9 @@ export default function Listing(props) {
     return (
         <div className='listing'>
             <div className="content">
-                <Divider />
-                    <h1> My Listing</h1>
-                <Divider />
+                <div className='header'>
+                  <h1> My Listing</h1>
+                </div>           
                 <div className='create-listing'>
                     <button onClick={()=> setShowCreateModal(true)}>Create New Listing</button>
                     <CreateListingModal showCreateModal={showCreateModal} setShowCreateModal={setShowCreateModal}/>
