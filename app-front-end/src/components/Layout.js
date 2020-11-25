03adf4e3bb0e228c062a4d83adb73ac13b5b82e5
[nick_fasulo] <[nick.fasulo@codeimmersives]>
2020-11-25 13:00:35 -0500

    minor change

--- app-front-end/src/components/Layout.js
+++ app-front-end/src/components/Layout.js
@@ -1,50 +1,55 @@
 import React, { useState } from 'react';
-import Header from './header/Header'
-import HistoryBar from './historyBar/HistoryBar'
-import '../css/layout.scss'
+import Header from './header/Header';
+import HistoryBar from './historyBar/HistoryBar';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+import '../css/layout.scss';
 
-export default function Layout (props) {
-    const [logIn , setLogIn] = useState(false)
-    const loginHandler = () => setLogIn(true)
-    const [sidebarToggle,setSidebarToggle] =useState(false)
- 
-    
-///////////////////// side bar arrow switch
-    let sideBar
-    let arrowToggle
-  if (sidebarToggle){
-    sideBar = (    
+export default function Layout(props) {
+  const [setLogIn] = useState(false);
+  const loginHandler = () => setLogIn(true);
+  const [sidebarToggle, setSidebarToggle] = useState(false);
+
+  ///////////////////// side bar arrow switch
+  let sideBar;
+  let arrowToggle;
+  if (sidebarToggle) {
+    sideBar = (
       <React.Fragment>
-          <HistoryBar/>
+        <HistoryBar />
       </React.Fragment>
-      )
-    }else{
-        sideBar = (
-        <React.Fragment>
-            <div>
-            </div>
-        </React.Fragment>
-      )
-        }
-////////////////////////
- 
-        return (
-            <div className='Main'>
-                <Header 
-                loginHandler={loginHandler}
+    );
+  } else {
+    sideBar = (
+      <React.Fragment>
+        <div></div>
+      </React.Fragment>
+    );
+  }
+  ////////////////////////
 
-                />
-                    <div className='page-item'>
-                        <div className='page-content'>
-                            {props.children}
-                            <div className='sidebar-toggle'>
-                                {sidebarToggle ? <ArrowForwardIosIcon className='arrow' onClick={() => setSidebarToggle(false)}/> :<ArrowBackIosIcon className='arrow' onClick={() => setSidebarToggle(true)}/>}
-                            </div>
-                        </div> 
-                        {sideBar}
-                    </div>
-            </div>
-        )
+  return (
+    <div className="Main">
+      <Header loginHandler={loginHandler} />
+      <div className="page-item">
+        <div className="page-content">
+          {props.children}
+          <div className="sidebar-toggle">
+            {sidebarToggle ? (
+              <ArrowForwardIosIcon
+                className="arrow"
+                onClick={() => setSidebarToggle(false)}
+              />
+            ) : (
+              <ArrowBackIosIcon
+                className="arrow"
+                onClick={() => setSidebarToggle(true)}
+              />
+            )}
+          </div>
+        </div>
+        {sideBar}
+      </div>
+    </div>
+  );
 }
