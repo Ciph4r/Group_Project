0204347ba95cb7651ba3a898d2d40d07b1b7e133
Dave.L <59891341+Ciph4r@users.noreply.github.com>
2020-12-04 15:21:02 -0500

    fixed messagebox

--- app-front-end/src/store/actions/inbox.js
+++ app-front-end/src/store/actions/inbox.js
@@ -19,6 +19,26 @@ export const fetchInbox = createAsyncThunk(
     }
   );
 
+  export const sendMsg = createAsyncThunk(
+    'sendMsg',
+    async (args, thunkAPI) => {
+        console.log(args)
+        const token = thunkAPI.getState().user.token;
+        const createUrl = `${process.env.REACT_APP_API_URL}/api/inbox/sendmsg/:${args.id}`;
+        const response = await fetch(createUrl, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`,
+              },
+      });
+      let jsonData = await response.json();
+        // return jsonData.inbox
+        console.log(jsonData)
+    }
+  );
+
+
 export const changetoRead = (id) => {
     return {type: 'changetoRead' , payload:id};
     };
--- app-front-end/src/store/reducer/InboxReducer.js
+++ app-front-end/src/store/reducer/InboxReducer.js
@@ -70,45 +70,6 @@ export const inboxSlice = createSlice({
 });
 
 
-
-// const initialstate = [
-//     {   id: 123456789,
-//         sender:'john',
-//         receiver:'dave',
-//         read:false,
-//         date:'11:45 PM',
-//         img: 'https://c7.alamy.com/comp/P9MYWR/man-avatar-profile-P9MYWR.jpg',
-//         messages:[
-//             {
-//             name: 'john',
-//             message: 'Hi'
-//             },
-//             {
-//             name: 'dave',
-//             message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-//             }
-//         ]
-
-//     },
-//     {   id: 987654321,
-//         sender:'john',
-//         receiver:'andy',
-//         read:true,
-//         date:'11:45 PM',
-//         img: 'https://c7.alamy.com/comp/P9MYWR/man-avatar-profile-P9MYWR.jpg',
-//         messages:[
-//             {
-//             name: 'john',
-//             message: 'Hi'
-//             },
-//             {
-//             name: 'andy',
-//             message: 'Hi Back'
-//             }
-//         ]
-
-//     }
-//   ]
   
 //     export const inboxReducer = (state = initialstate , action) => {
 //         if(action.type === 'changetoRead'){
