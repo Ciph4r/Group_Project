b667e8d30bccc83c33ee27f41988bb2a86402043
[nick_fasulo] <[nick.fasulo@codeimmersives]>
2020-12-11 16:23:07 -0500

    adjusted listing css

--- app-front-end/src/components/Listing.js
+++ app-front-end/src/components/Listing.js
@@ -1,49 +1,54 @@
 import React, { useState } from 'react';
-import '../css/listing.scss';
-import 'react-calendar/dist/Calendar.css';
-import {CreateListingModal} from './CreateListingModal';
+import { CreateListingModal } from './CreateListingModal';
 import { useSelector } from 'react-redux';
-import {ListingCard} from './listingCard';
-
-
+import { ListingCard } from './listingCard';
+import 'react-calendar/dist/Calendar.css';
+import '../css/listing.scss';
 
 
 
 export default function Listing(props) {
-    const user =useSelector((state) => state.user.user_id);
-    const carData = useSelector((state) => state.car.cars).filter(car => car.owner === user);
-    const [showCreateModal, setShowCreateModal] = useState(false);
-    const [dataState , setData]= useState();
+  const user = useSelector(state => state.user.user_id);
+  const carData = useSelector(state => state.car.cars).filter(
+    car => car.owner === user
+  );
+  const [showCreateModal, setShowCreateModal] = useState(false);
+  const [dataState, setData] = useState();
 
-    const modalType = (id) => {
-      setData(id);
-      setShowCreateModal(true);
-    };
-
-    return (
-        <div className='listing'>
-            <div className="content">
-                <div className='header'>
-                    <h1> My Listing</h1>
-                </div>           
-                <div className='create-listing'>
-                    <button onClick={()=> modalType(false)}>Create New Listing</button>
-                    <CreateListingModal showCreateModal={showCreateModal} setShowCreateModal={setShowCreateModal} id={dataState}  />
-                </div>
-
-           
-            {carData.length > 0 ?
-            <div className='main-content'>
-                {carData.map((data, key) => (
-                <ListingCard data={data} modalType = {modalType} key = {key} setShowCreateModal = {setShowCreateModal}/>
-                                    // ListingCard(data ,modalType ,key ,setShowCreateModal)
-                ))}
-             </div>
-             : 
-             <h1>You have No Listing</h1>}
-             </div>
-        </div>
-      
-    );
+  const modalType = id => {
+    setData(id);
+    setShowCreateModal(true);
   };
-  
+
+  return (
+    <div className="listing">
+      <div className="header">
+        <h1> My Listing</h1>
+      </div>
+      <div className="create-listing">
+        <button onClick={() => modalType(false)}>Create New Listing</button>
+        <CreateListingModal
+          showCreateModal={showCreateModal}
+          setShowCreateModal={setShowCreateModal}
+          id={dataState}
+        />
+      </div>
+
+      {carData.length > 0 ? (
+        <div className="main-content">
+          {carData.map((data, key) => (
+            <ListingCard
+              data={data}
+              modalType={modalType}
+              key={key}
+              setShowCreateModal={setShowCreateModal}
+            />
+            // ListingCard(data ,modalType ,key ,setShowCreateModal)
+          ))}
+        </div>
+      ) : (
+        <h1>You have No Listing</h1>
+      )}
+    </div>
+  );
+}
